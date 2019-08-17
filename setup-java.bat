d626be8601f755d38e91b4a132e88b7c8ccc5b60
Aaron Kunde <aaron.kunde@web.de>
2019-08-17 17:06:33 +0200

    Added scripts for Java

---
+++ setup-java.bat
@@ -0,0 +1,3 @@
+set JAVA_HOME=%USERPROFILE%\opt\jdk-8u92-windows-x64
+set M2_HOME=%USERPROFILE%\opt\apache-maven-3.2.3
+set PATH=%PATH%;%JAVA_HOME%\bin;%M2_HOME%\bin
---
+++ setup-java.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+JAVA_HOME="$HOME/opt/jdk-8u92-windows-x64"
+PATH="$PATH:$JAVA_HOME/bin"
+
+exec "$BASH" --login 
+
