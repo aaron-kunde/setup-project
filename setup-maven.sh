b58eedcbe1e473e6087b319d6a0bf17b1a523088
Aaron Kunde <aaron.kunde@web.de>
2019-09-07 12:12:42 +0200

    Renamed variable to VERSION

--- setup-maven.sh
+++ setup-maven.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-MVN_VERSION=3.5.4
+VERSION=3.5.4
 test $JAVA_HOME || . $HOME/bin/setup-java.sh -i
 
 while getopts sv:j: opt; do
     case $opt in
 	s) do_exec_bash=false
 	   ;;
-	v) MVN_VERSION=$OPTARG
+	v) VERSION=$OPTARG
 	   ;;
 	j) JAVA_HOME=$OPTARG
 	   PATH="$PATH:$JAVA_HOME/bin"
@@ -16,13 +16,13 @@ while getopts sv:j: opt; do
 done
 
 
-M2_HOME="$HOME/opt/apache-maven-$MVN_VERSION"
+M2_HOME="$HOME/opt/apache-maven-$VERSION"
 
 if [ ! -d $M2_HOME ]; then
-    install_file="$HOME/Downloads/apache-maven-$MVN_VERSION-bin.tar.gz"
+    install_file="$HOME/Downloads/apache-maven-$VERSION-bin.tar.gz"
 
     if [ ! -f $install_file ]; then
-	url=http://ftp.halifax.rwth-aachen.de/apache/maven/maven-3/$MVN_VERSION/binaries/apache-maven-$MVN_VERSION-bin.tar.gz
+	url=http://ftp.halifax.rwth-aachen.de/apache/maven/maven-3/$VERSION/binaries/apache-maven-$VERSION-bin.tar.gz
 	curl $url -o $install_file
     fi
     tar zxf $install_file -C "$HOME/opt"
