21b06cdbdc1cf40f8ca9b67dad5a8fc95c0ad973
Aaron Kunde <aaron.kunde@web.de>
2019-09-07 12:13:08 +0200

    Made Gradle setup similar to Maven

--- setup-gradle.sh
+++ setup-gradle.sh
@@ -1,9 +1,34 @@
 #!/bin/sh
 
-export JAVA_HOME="$HOME/opt/jdk-8u92-windows-x64"
-PATH="$PATH:$JAVA_HOME/bin"
+VERSION=5.6.2
+test $JAVA_HOME || . $HOME/bin/setup-java.sh -i
 
-GRADLE_HOME="$HOME/opt/gradle-4.6"
-PATH="$PATH:$GRADLE_HOME/bin"
+while getopts sv:j: opt; do
+    case $opt in
+	s) do_exec_bash=false
+	   ;;
+	v) VERSION=$OPTARG
+	   ;;
+	j) JAVA_HOME=$OPTARG
+	   PATH="$PATH:$JAVA_HOME/bin"
+	   ;;
+    esac
+done
 
-exec "$BASH" --login
+
+GRADLE_HOME="$HOME/opt/gradle-$VERSION"
+
+if [ ! -d $GRADLE_HOME ]; then
+    install_file="$HOME/Downloads/gradle-$VERSION-bin.zip"
+
+    if [ ! -f $install_file ]; then
+	url=https://services.gradle.org/distributions/gradle-$VERSION-bin.zip
+	curl -L $url -o $install_file
+    fi
+    unzip -d "$HOME/opt" "$install_file"
+fi
+
+export PATH="$PATH:$GRADLE_HOME/bin"
+export ORIGINAL_PATH="${PATH}"
+
+exec "$BASH" --login -i
