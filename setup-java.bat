47940698de40f295e20cb9a59706a80890f788e7
Aaron Kunde <aaron.kunde@web.de>
2020-01-30 09:43:31 +0100

    Setting correct java version

--- setup-java.bat
+++ setup-java.bat
@@ -1,4 +1,4 @@
 rem TODO: https://www.whitebyte.info/programming/java/how-to-install-a-portable-jdk-in-windows-without-admin-rights
-set JAVA_VERSION=1.8.0_92
+set JAVA_VERSION=1.8.0_92-b14
 set JAVA_HOME=%USERPROFILE%\opt\jdk%JAVA_VERSION%
 set PATH=%PATH%;%JAVA_HOME%\bin
