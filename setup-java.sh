1fc57844ec284cfc841a66a54902730db7c30c76
Aaron Kunde <aaron.kunde@web.de>
2020-04-13 13:54:58 +0200

    More distinct use of global and local variables

--- setup-java.sh
+++ setup-java.sh
@@ -2,28 +2,30 @@
 
 set -e
 
-default_version=8u222-b10
-default_provider=adoptopenjdk
+DEFAULT_VERSION=8u222-b10
+DEFAULT_PROVIDER=adoptopenjdk
 
 
 print_usage() {
     echo "${0} [-p PROVIDER] [-v VERSION] [-i]"
     echo "  -p PROVIDER Provider for JDK binaries"
     echo "     Possible values are: oracle or adoptopenjdk"
-    echo "     Default: $default_provider"
+    echo "     Default: $DEFAULT_PROVIDER"
     echo "  -v VERSION Version of the JDK"
-    echo "     Default: $default_version"
+    echo "     Default: $DEFAULT_VERSION"
     echo "  -i Does not execute a new login shell. This can be used, to import"
     echo "     this script in other scripts"
 }
 
 oracle_export_variables() {
+    version=${1}
     export JAVA_HOME="$HOME/opt/jdk$version"
     export PATH="$PATH:$JAVA_HOME/bin"
     export ORIGINAL_PATH="${PATH}"
 }
 
 oracle_short_version() {
+    version=${1}
     tmp=${version:2:-4}
     echo ${tmp/.0_/u}
 }
@@ -39,6 +41,8 @@ oracle_check_install_file() {
 }    
 
 oracle_install_jdk() {
+    version=${1}
+    
     if [ ! -d $JAVA_HOME ]; then
 	short_version=$(oracle_short_version $version)
 	download_dir=$HOME/Downloads
@@ -64,11 +68,14 @@ oracle_install_jdk() {
 
 
 adoptopenjdk_major_version() {
-    echo "${1}" | sed -ne "s/^\([0-9]\+\).*/\1/p" 
+    version=${1}
+    echo $version | sed -ne "s/^\([0-9]\+\).*/\1/p" 
 }
 
 adoptopenjdk_export_variables() {
+    version=${1}
     major_version=$(adoptopenjdk_major_version $version)
+
     if [ $major_version -gt 8 ]; then
 	export JAVA_HOME="$HOME/opt/jdk-$version"
     else
@@ -79,18 +86,25 @@ adoptopenjdk_export_variables() {
     export ORIGINAL_PATH="${PATH}"
 }
 
+adoptopenjdk_short_version() {
+    version=${1}
+    echo ${version/-/}
+}
+
 adoptopenjdk_install_jdk() {
+    version=${1}
+
     if [ ! -d $JAVA_HOME ]; then
-	short_version=${version/-/}
+	short_version=$(adoptopenjdk_short_version $version)
 	download_dir=$HOME/Downloads
 	install_file=$download_dir/OpenJDK8U-jdk_x64_windows_hotspot_$short_version.zip
 	install_sha256_file=$install_file.sha256
 	url=https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk$version
 	if [ ! -f $download_dir/$install_file ]; then
-	    curl -L $url/$install_file -o $install_file
+	    echo curl -L $url/$install_file -o $install_file
 	fi
 	if [ ! -f $download_dir/$install_sha256_file ]; then
-	    curl -L $url/$install_sha256_file.txt -o $install_sha256_file
+	    echo curl -L $url/$install_sha256_file.txt -o $install_sha256_file
 	fi
 	pushd $HOME/Downloads
 	sha256sum -c $install_sha256_file
@@ -114,20 +128,20 @@ while getopts iv:p: opt; do
     esac
 done
 
-case ${provider:-$default_provider} in
+case ${provider:-$DEFAULT_PROVIDER} in
     oracle)
-	default_version=1.8.0_92-b14	
-	version=${version:-$default_version}
+	DEFAULT_VERSION=1.8.0_92-b14	
+	version=${version:-$DEFAULT_VERSION}
 	echo "Setup Oracle JDK $version"
-	oracle_export_variables
-	oracle_install_jdk
+	oracle_export_variables $version
+	oracle_install_jdk $version
 	java -version
 	;;
     adoptopenjdk)
-	version=${version:-$default_version}
+	version=${version:-$DEFAULT_VERSION}
 	echo "Setup AdoptOpenJDK $version"
-	adoptopenjdk_export_variables
-	adoptopenjdk_install_jdk
+	adoptopenjdk_export_variables $version
+	adoptopenjdk_install_jdk $version
 	java -version
 	;;
     *)
