28474891a08fe5561322db76913c675f68c348d2
Aaron Kunde <aaron.kunde@web.de>
2021-04-04 15:29:12 +0200

    No login bash anymore

--- setup-maven.sh
+++ setup-maven.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-VERSION=3.6.3
-test $JAVA_HOME || . $HOME/bin/setup-java.sh -i
+lib_dir=$(dirname ${BASH_SOURCE})
+test $JAVA_HOME || . $lib_dir/setup-java.sh
 
-while getopts isv:j: opt; do
+version=3.6.3
+
+while getopts v:j: opt; do
     case $opt in
-	i) skip_exec_bash=true
-	   ;;
-	s) do_exec_bash=false
-	   ;;
-	v) VERSION=$OPTARG
+	v) version=$OPTARG
 	   ;;
 	j) JAVA_HOME=$OPTARG
 	   PATH="$PATH:$JAVA_HOME/bin"
@@ -17,14 +15,13 @@ while getopts isv:j: opt; do
     esac
 done
 
-
-M2_HOME="$HOME/opt/apache-maven-$VERSION"
+M2_HOME="$HOME/opt/apache-maven-$version"
 
 if [ ! -d $M2_HOME ]; then
-    install_file="/tmp/apache-maven-$VERSION-bin.tar.gz"
+    install_file="/tmp/apache-maven-$version-bin.tar.gz"
 
     if [ ! -f $install_file ]; then
-	url=http://ftp.halifax.rwth-aachen.de/apache/maven/maven-3/$VERSION/binaries/apache-maven-$VERSION-bin.tar.gz
+	url=http://ftp.halifax.rwth-aachen.de/apache/maven/maven-3/$version/binaries/apache-maven-$version-bin.tar.gz
 	curl $url -o $install_file
     fi
     tar zxf $install_file -C "$HOME/opt"
@@ -32,7 +29,3 @@ fi
 
 export PATH="$PATH:$M2_HOME/bin"
 export ORIGINAL_PATH="${PATH}"
-
-if [ ! $skip_exec_bash ]; then
-    exec "$BASH" --login -i
-fi
