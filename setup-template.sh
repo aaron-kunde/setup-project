77b70d2b1c17ff61b5c669f73c90e74322f1d529
Aaron Kunde <aaron.kunde@web.de>
2022-12-20 23:31:02 +0100

    Add template and first tests

---
+++ setup-template.sh
@@ -0,0 +1,168 @@
+# Specific implementation needed
+init_global_vars() {
+    DEFAULT_TMPL_VERSION=default_version
+    TMPL_VERSION=$DEFAULT_TMPL_VERSION
+    INSTALLATION_BASE_DIR=$HOME/opt
+    # Reset OPTIND, if getopts was used before
+    OPTIND=1
+}
+
+# Specific implementation needed
+print_usage() {
+    cat <<EOM
+${0} [-v VERSION]
+     -v VERSION Version of TMPL to install.
+     	Default: $DEFAULT_TMPL_VERSION
+EOM
+}
+
+# Specific implementation needed
+set_vars_from_opts() {
+    while getopts v: opt; do
+	case $opt in
+	    v) TMPL_VERSION=$OPTARG
+	       ;;
+	esac
+    done
+}
+
+# Specific implementation needed
+installation_path() {
+    # There might be different installation paths, depending on target OS
+    case "$(uname -s)" in
+	CYGWIN*|MINGW*|MSYS*)
+	    echo $INSTALLATION_BASE_DIR/tmpl-$TMPL_VERSION
+	    ;;
+	*)
+	    echo $INSTALLATION_BASE_DIR/tmpl-$TMPL_VERSION
+	    ;;
+    esac
+}
+
+# Specific implementation needed
+export_path_vars() {
+    echo "Adding $(installation_path) to PATH"
+    SETUP_TMPL_ORIGINAL_PATH="${PATH}"
+    
+    export PATH="$(installation_path):${PATH}"
+}
+
+# Specific implementation needed
+reset_path_vars() {
+    if [ -v SETUP_TMPL_ORIGINAL_PATH ]; then
+	export PATH="${SETUP_TMPL_ORIGINAL_PATH}"
+	unset SETUP_TMPL_ORIGINAL_PATH
+    fi
+}
+
+# Specific implementation needed
+reset_global_vars() {
+    unset TMPL_VERSION
+    unset DEFAULT_TMPL_VERSION
+    unset INSTALLATION_BASE_DIR
+    # Reset OPTIND for future use of getopts
+    OPTIND=1
+}
+
+abort() {
+    reset_path_vars
+    reset_global_vars
+
+    return 0
+}
+
+# Specific implementation needed
+is_installed() {
+    case "$TMPL_VERSION" in
+	installed) return 0
+	    ;;
+	*) return 1
+	    ;;
+    esac
+}
+
+# Specific implementation needed
+installation_file() {
+    # There might be different installation files, depending on target OS
+    case "$(uname -s)" in
+	CYGWIN*|MINGW*|MSYS*)
+	    echo installation.file
+	    ;;
+	*)
+	    echo installation.file
+	    ;;
+    esac
+}
+
+local_installation_file() {
+    echo /tmp/$(installation_file)
+}
+
+local_installation_file_exists() {
+    test -f $(local_installation_file)
+}
+
+# Specifc implermentation needed
+download_url() {
+    case "$TMPL_VERSION" in
+	download_fail) echo https://github.com/aaron-kunde/setup-project/blob/master/non-existing.file
+	   ;;
+	*) echo https://github.com/aaron-kunde/setup-project/blob/master/README.org
+	   ;;
+    esac
+}
+
+remote_installation_file_exists() {
+    curl -sIf $(download_url) >/dev/null
+}
+
+download_installation_file() {
+    echo "Download installation file" 
+    curl $(download_url) -o $(local_installation_file)
+}
+
+# Specific implementation needed
+install_installation_file() {
+    echo "Install installation file"
+        case "$TMPL_VERSION" in
+	installation_fail) return 1
+	   ;;
+	*) return 0
+	   ;;
+    esac
+}
+
+# TODO: Really specific?
+# Specific implementation needed
+install() {
+    echo "Install new TMPL: $TMPL_VERSION"
+
+    if ! local_installation_file_exists; then
+	echo "Local installation file not found: $(installation_file). Try, download new one"
+	if remote_installation_file_exists; then
+	    download_installation_file
+	else
+	    echo "ERROR: No installation file found. Abort"
+	    abort
+	fi
+    fi
+    install_installation_file
+}
+
+
+
+
+
+
+init_global_vars
+reset_path_vars
+set_vars_from_opts ${@}
+export_path_vars
+
+if ! is_installed; then
+    install || abort
+fi
+
+echo "TMPL installed"
+# Reset OPTIND, if getopts was used before
+OPTIND=1
---
+++ test-template.sh
@@ -0,0 +1,202 @@
+#! /bin/sh
+
+set -e
+
+SPT_ORIGINAL_PATH="$PATH"
+INSTALLATION_FILE=/tmp/installation.file
+
+assert() {
+    local assertion=${1}
+    local msg=${2}
+
+    # Must not be quoted!
+    test $assertion || (
+	echo "Assertion fail: $msg"
+	exit  1
+    )
+}
+
+it() {
+    echo -e "\nTEST: ${1}\n"
+
+    ${1}
+
+    if [ $? ]; then
+	echo -e "\n-> SUCCESS\n"
+	return 0
+    else
+	echo -e "\n-> FAIL\n"
+	return 1
+    fi
+}
+
+assert_clean_env() {
+    assert "$OPTIND -eq 1" \
+	   "OPTIND != 1: $OPTIND"
+    assert "-z $INSTALLATION_BASE_DIR" \
+	   "INSTALLATION_BASE_DIR is set: $INSTALLATION_BASE_DIR"
+    assert "-z $DEFAULT_TMPL_VERSION" \
+	   "DEFAULT_TMPL_VERSION is set: $DEFAULT_TMPL_VERSION"
+    assert "-z $TMPL_VERSION" \
+	   "TMPL_VERSION is set: $TMPL_VERSION"
+    assert "-z $SETUP_TMPL_ORIGINAL_PATH" \
+	   "SETUP_TMPL_ORIGINAL_PATH is set: $SETUP_TMPL_ORIGINAL_PATH"
+    assert "! -e $INSTALLATION_FILE" \
+	   "Installation file exists"
+}
+
+should_successfully_use_default_version_if_no_version_given() {
+    assert_clean_env
+    
+    . ./setup-template.sh
+    
+    assert "$OPTIND -eq 1" \
+	   "OPTIND != 1: $OPTIND" 
+    assert "$INSTALLATION_BASE_DIR = $HOME/opt" \
+	   "INSTALLATION_BASE_DIR != HOME/opt: $INSTALLATION_BASE_DIR"
+    assert "$DEFAULT_TMPL_VERSION = default_version" \
+	   "DEFAULT_TMPL_VERSION != default_version: $DEFAULT_TMPL_VERSION"
+    assert "$TMPL_VERSION = default_version" \
+	   "TMPL_VERSION != default_version: $TMPL_VERSION"
+    assert "${SETUP_TMPL_ORIGINAL_PATH// /§} = ${SPT_ORIGINAL_PATH// /§}" \
+	   "SETUP_TMPL_ORIGINAL_PATH != SPT_ORIGINAL_PATH: $SETUP_TMPL_ORIGINAL_PATH"
+    assert "${PATH// /§} = ${INSTALLATION_BASE_DIR// /§}/tmpl-default_version:${SPT_ORIGINAL_PATH// /§}" \
+	   "PATH is not correct: $PATH"
+    assert "-e $INSTALLATION_FILE" \
+	   "Installation file should exist"
+	
+    reset_path_vars
+    reset_global_vars
+    rm "$INSTALLATION_FILE"
+}
+
+should_successfully_use_given_version() {
+    assert_clean_env
+    
+    . ./setup-template.sh -v0
+    
+    assert "$OPTIND -eq 1" \
+	   "OPTIND != 1: $OPTIND" 
+    assert "$INSTALLATION_BASE_DIR = $HOME/opt" \
+	   "INSTALLATION_BASE_DIR != HOME/opt: $INSTALLATION_BASE_DIR"
+    assert "$DEFAULT_TMPL_VERSION = default_version" \
+	   "DEFAULT_TMPL_VERSION != default_version: $DEFAULT_TMPL_VERSION"
+    assert "$TMPL_VERSION = 0" \
+	   "TMPL_VERSION != 0: $TMPL_VERSION"
+    assert "${SETUP_TMPL_ORIGINAL_PATH// /§} = ${SPT_ORIGINAL_PATH// /§}" \
+	   "SETUP_TMPL_ORIGINAL_PATH != SPT_ORIGINAL_PATH: $SETUP_TMPL_ORIGINAL_PATH"
+    assert "${PATH// /§} = ${INSTALLATION_BASE_DIR// /§}/tmpl-0:${SPT_ORIGINAL_PATH// /§}" \
+	   "PATH is not correct: $PATH"
+    assert "-e $INSTALLATION_FILE" \
+	   "Installation file should exist"
+    
+    reset_path_vars
+    reset_global_vars
+    rm "$INSTALLATION_FILE"
+}
+
+should_skip_installation_and_set_paths_if_already_installed() {
+    assert_clean_env
+    
+    . ./setup-template.sh -vinstalled
+    
+    assert "$OPTIND -eq 1" \
+	   "OPTIND != 1: $OPTIND" 
+    assert "$INSTALLATION_BASE_DIR = $HOME/opt" \
+	   "INSTALLATION_BASE_DIR != HOME/opt: $INSTALLATION_BASE_DIR"
+    assert "$DEFAULT_TMPL_VERSION = default_version" \
+	   "DEFAULT_TMPL_VERSION != default_version: $DEFAULT_TMPL_VERSION"
+    assert "$TMPL_VERSION = installed" \
+	   "TMPL_VERSION != installed: $TMPL_VERSION"
+    assert "${SETUP_TMPL_ORIGINAL_PATH// /§} = ${SPT_ORIGINAL_PATH// /§}" \
+	   "SETUP_TMPL_ORIGINAL_PATH != SPT_ORIGINAL_PATH: $SETUP_TMPL_ORIGINAL_PATH"
+    assert "${PATH// /§} = ${INSTALLATION_BASE_DIR// /§}/tmpl-installed:${SPT_ORIGINAL_PATH// /§}" \
+	   "PATH is not correct: $PATH"
+    assert "! -e $INSTALLATION_FILE" \
+	   "Installation file should not exist"
+    
+    reset_path_vars
+    reset_global_vars
+}
+
+should_skip_download_and_install_if_installation_file_exists_local() {
+    assert_clean_env
+    touch "$INSTALLATION_FILE"
+
+    . ./setup-template.sh -v0
+
+    assert "$OPTIND -eq 1" \
+	   "OPTIND != 1: $OPTIND" 
+    assert "$INSTALLATION_BASE_DIR = $HOME/opt" \
+	   "INSTALLATION_BASE_DIR != HOME/opt: $INSTALLATION_BASE_DIR"
+    assert "$DEFAULT_TMPL_VERSION = default_version" \
+	   "DEFAULT_TMPL_VERSION != default_version: $DEFAULT_TMPL_VERSION"
+    assert "$TMPL_VERSION = 0" \
+	   "TMPL_VERSION != 0: $TMPL_VERSION"
+    assert "${SETUP_TMPL_ORIGINAL_PATH// /§} = ${SPT_ORIGINAL_PATH// /§}" \
+	   "SETUP_TMPL_ORIGINAL_PATH != SPT_ORIGINAL_PATH: $SETUP_TMPL_ORIGINAL_PATH"
+    assert "${PATH// /§} = ${INSTALLATION_BASE_DIR// /§}/tmpl-0:${SPT_ORIGINAL_PATH// /§}" \
+	   "PATH is not correct: $PATH"
+    assert "-e $INSTALLATION_FILE" \
+	   "Installation file should exist"
+    
+    reset_path_vars
+    reset_global_vars
+    rm "$INSTALLATION_FILE"
+}
+
+should_abort_installation_if_download_fails() {
+    assert_clean_env
+
+    . ./setup-template.sh -vdownload_fail
+
+    assert "$OPTIND -eq 1" \
+	   "OPTIND != 1: $OPTIND" 
+    assert "-z $INSTALLATION_BASE_DIR" \
+	   "INSTALLATION_BASE_DIR is set: $INSTALLATION_BASE_DIR"
+    assert "-z $DEFAULT_TMPL_VERSION" \
+	   "DEFAULT_TMPL_VERSION is set: $DEFAULT_TMPL_VERSION"
+    assert "-z $TMPL_VERSION" \
+	   "TMPL_VERSION is set: $TMPL_VERSION"
+    assert "-z $SETUP_TMPL_ORIGINAL_PATH" \
+	   "SETUP_TMPL_ORIGINAL_PATH is set: $SETUP_TMPL_ORIGINAL_PATH"
+    assert "${PATH// /§} = ${SPT_ORIGINAL_PATH// /§}" \
+	   "PATH is not correct: $PATH"
+    assert "! -e $INSTALLATION_FILE" \
+	   "Installation file should not exist"
+    
+    reset_path_vars
+    reset_global_vars
+}
+
+should_abort_installation_if_installation_fails() {
+    assert_clean_env
+
+    . ./setup-template.sh -vinstallation_fail
+
+    assert "$OPTIND -eq 1" \
+	   "OPTIND != 1: $OPTIND" 
+    assert "-z $INSTALLATION_BASE_DIR" \
+	   "INSTALLATION_BASE_DIR is set: $INSTALLATION_BASE_DIR"
+    assert "-z $DEFAULT_TMPL_VERSION" \
+	   "DEFAULT_TMPL_VERSION is set: $DEFAULT_TMPL_VERSION"
+    assert "-z $TMPL_VERSION" \
+	   "TMPL_VERSION is set: $TMPL_VERSION"
+    assert "-z $SETUP_TMPL_ORIGINAL_PATH" \
+	   "SETUP_TMPL_ORIGINAL_PATH is set: $SETUP_TMPL_ORIGINAL_PATH"
+    assert "${PATH// /§} = ${SPT_ORIGINAL_PATH// /§}" \
+	   "PATH is not correct: $PATH"
+    assert "-e $INSTALLATION_FILE" \
+	   "Installation file should exist"
+
+    reset_path_vars
+    reset_global_vars
+    rm "$INSTALLATION_FILE"
+}
+
+it should_successfully_use_default_version_if_no_version_given
+it should_successfully_use_given_version
+it should_skip_installation_and_set_paths_if_already_installed
+it should_skip_download_and_install_if_installation_file_exists_local
+it should_abort_installation_if_download_fails
+it should_abort_installation_if_installation_fails
