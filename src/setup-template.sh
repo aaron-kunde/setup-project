dceb5eea7e620d411bfc2ecfec623774893870f4
Aaron Kunde <aaron.kunde@web.de>
2024-05-31 16:27:32 +0200

    Remove function print_usage

--- src/setup-template.sh
+++ src/setup-template.sh
@@ -2,13 +2,6 @@
 default_version() {
     echo tmpl_default-version
 }
-print_usage() {
-    cat <<EOM
-${0} [-v VERSION]
-     -v VERSION Version of TMPL to install.
-	Default: $(default_version)
-EOM
-}
 init_global_vars() {
     VERSION=$(default_version)
     INSTALLATION_BASE_DIR=$HOME/opt
@@ -51,7 +44,6 @@ restore_exported_vars
 export_vars
 
 echo "TODO: Not yet implemented"
-print_usage
 
 reset_global_vars
 echo "TMPL successfully installed"
